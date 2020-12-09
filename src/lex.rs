274245ea384006f8b3f9536d0799398b8adfa98c
Fred Morcos <fm@fredmorcos.com>
2020-12-09 22:28:25 +0100

    Numerical addresses

--- src/lex.rs
+++ src/lex.rs
@@ -1,6 +1,9 @@
+use std::convert::TryFrom;
+
+use atoi::FromRadix10Checked;
 // use crate::inst::Dest;
-use crate::inst::Inst;
 use crate::inst::Label;
+use crate::inst::{Addr, Inst};
 // use crate::inst::{Comp, Label};
 use crate::loc::Loc;
 use crate::loc::SrcLoc;
@@ -9,12 +12,13 @@ use crate::srcloc;
 pub struct Lex<'b> {
   buf: &'b [u8],
   index: usize,
+  len: usize,
   loc: Loc,
 }
 
-impl<'b> Lex<'b> {
-  pub fn new(buf: &'b [u8]) -> Self {
-    Self { buf, index: usize::default(), loc: Loc::default() }
+impl<'b> From<&'b [u8]> for Lex<'b> {
+  fn from(buf: &'b [u8]) -> Self {
+    Self { buf, index: usize::default(), len: usize::default(), loc: Loc::default() }
   }
 }
 
@@ -24,6 +28,7 @@ pub enum ErrKind {
   CommentByte(u8),
   AddrEOF,
   AddrNumByte(u8),
+  AddrOutOfRange(u16),
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -48,11 +53,26 @@ impl<'b> Iterator for Lex<'b> {
   fn next(&mut self) -> Option<Self::Item> {
     macro_rules! next {
       ($on_eof:block) => {
-        if let Some(&c) = self.buf.get(0) {
-          self.buf = &self.buf[1..];
+        if let Some(&c) = self.buf.get(self.index) {
+          self.index += 1;
+          self.len += 1;
           self.loc.inc(c);
           c
         } else $on_eof
+      };
+    }
+
+    macro_rules! tok {
+      () => {{
+        let tok = &self.buf[self.index..self.index + self.len];
+        self.len = 0;
+        tok
+      }};
+    }
+
+    macro_rules! err {
+      ($loc:expr, $err:expr) => {
+        Some(Err(Err::new(srcloc!(), $loc, $err)))
       };
     }
 
@@ -69,13 +89,10 @@ impl<'b> Iterator for Lex<'b> {
         }
       } else if c == b'/' {
         let loc = self.loc;
-
-        c = next!({
-          return Some(Err(Err::new(srcloc!(), loc, ErrKind::CommentEOF)));
-        });
+        c = next!({ return err!(loc, ErrKind::CommentEOF) });
 
         if c != b'/' {
-          return Some(Err(Err::new(srcloc!(), loc, ErrKind::CommentByte(c))));
+          return err!(loc, ErrKind::CommentByte(c));
         }
 
         loop {
@@ -87,25 +104,28 @@ impl<'b> Iterator for Lex<'b> {
         }
       } else if c == b'@' {
         let loc = self.loc;
-        c = next!({ return Some(Err(Err::new(srcloc!(), loc, ErrKind::AddrEOF))) });
+        c = next!({ return err!(loc, ErrKind::AddrEOF) });
 
-        let mut len = 1;
         if c.is_ascii_digit() {
           loop {
             c = next!({ break });
 
             if !c.is_ascii_digit() {
               if !c.is_ascii_whitespace() {
-                return Some(Err(Err::new(srcloc!(), loc, ErrKind::AddrNumByte(c))));
+                return err!(loc, ErrKind::AddrNumByte(c));
               }
 
               break;
             }
-
-            len += 1;
           }
 
-        // let tok = self.buf
+          let tok = tok!();
+          if let Some(addr) = u16::from_radix_10_checked(tok).0 {
+            match Addr::try_from(addr) {
+              Ok(addr) => return Some(Ok(Tok(loc, Inst::Addr(addr)))),
+              Err(_) => return err!(loc, ErrKind::AddrOutOfRange(addr)),
+            }
+          }
         } else if Label::is_label_start(c) {
         }
 
@@ -284,177 +304,182 @@ impl<'b> Iterator for Lex<'b> {
   }
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::Lex;
-//   use crate::inst::Comp;
-//   use crate::inst::Dest;
-//   use crate::inst::Inst;
-//   use crate::inst::Jump;
-//   use crate::inst::Text;
-//   use crate::loc;
-//   use crate::utils::loc::Loc;
-//   use std::io::Read;
+#[cfg(test)]
+mod tests {
+  use super::Lex;
+  use super::Tok;
+  use crate::inst::Addr;
+  use crate::inst::Inst;
+  use crate::loc;
+  use crate::loc::Loc;
+  //   use crate::inst::Comp;
+  //   use crate::inst::Dest;
+  //   use crate::inst::Inst;
+  //   use crate::inst::Jump;
+  //   use crate::inst::Text;
+  //   use crate::loc;
+  //   use crate::utils::loc::Loc;
+  //   use std::io::Read;
 
-//   macro_rules! lex {
-//     ($f:expr) => {
-//       Lex::from(include_bytes!(concat!("../../tests/data/", $f)).bytes())
-//     };
-//   }
+  macro_rules! lex {
+    ($f:expr) => {
+      Lex::from(&include_bytes!(concat!("../tests/data/", $f))[..])
+    };
+  }
 
-//   macro_rules! next {
-//     ($lex:expr, $inst:expr) => {
-//       assert_eq!($lex.next(), Some(Ok($inst)));
-//     };
-//   }
+  macro_rules! next {
+    ($lex:expr, $inst:expr) => {
+      assert_eq!($lex.next(), Some(Ok($inst)));
+    };
+  }
 
-//   #[test]
-//   fn empty() {
-//     let mut lex = lex!("empty");
-//     assert_eq!(lex.loc, Loc::new(1, 0));
-//     assert_eq!(lex.next(), None);
-//     assert_eq!(lex.loc, Loc::new(1, 0));
-//   }
+  #[test]
+  fn empty() {
+    let mut lex = lex!("empty");
+    assert_eq!(lex.loc, Loc::new(1, 0));
+    assert_eq!(lex.next(), None);
+    assert_eq!(lex.loc, Loc::new(1, 0));
+  }
 
-//   #[test]
-//   fn spaces() {
-//     let mut lex = lex!("spaces");
-//     assert_eq!(lex.next(), None);
-//     assert_eq!(lex.loc, Loc::new(5, 0));
-//   }
+  #[test]
+  fn spaces() {
+    let mut lex = lex!("spaces");
+    assert_eq!(lex.next(), None);
+    assert_eq!(lex.loc, Loc::new(5, 0));
+  }
 
-//   #[test]
-//   fn comments() {
-//     let mut lex = lex!("comments");
-//     assert_eq!(lex.next(), None);
-//     assert_eq!(lex.loc, Loc::new(5, 0));
-//   }
+  #[test]
+  fn comments() {
+    let mut lex = lex!("comments");
+    assert_eq!(lex.next(), None);
+    assert_eq!(lex.loc, Loc::new(5, 0));
+  }
 
-//   #[test]
-//   fn addr_nums() {
-//     let mut lex = lex!("addr_nums");
-//     next!(lex, Inst::AddrNum(loc!(3, 5), 8192));
-//     next!(lex, Inst::AddrNum(loc!(5, 1), 123));
-//     next!(lex, Inst::AddrNum(loc!(9, 5), 556));
-//     assert_eq!(lex.next(), None);
-//   }
+  #[test]
+  fn addr_nums() {
+    let mut lex = lex!("addr_nums");
+    next!(lex, Tok(loc!(3, 5), Inst::Addr(Addr::Num(8192))));
+    next!(lex, Tok(loc!(5, 1), Inst::Addr(Addr::Num(123))));
+    next!(lex, Tok(loc!(9, 5), Inst::Addr(Addr::Num(556))));
+    assert_eq!(lex.next(), None);
+  }
 
-//   macro_rules! label {
-//     ($label:path, $line:expr, $col:expr, $text:expr) => {
-//       $label(loc!($line, $col), Text::from_slice($text))
-//     };
-//   }
+  //   macro_rules! label {
+  //     ($label:path, $line:expr, $col:expr, $text:expr) => {
+  //       $label(loc!($line, $col), Text::from_slice($text))
+  //     };
+  //   }
 
-//   #[test]
-//   fn address_labels() {
-//     let mut lex = lex!("addr_labels");
-//     next!(lex, label!(Inst::AddrLabel, 3, 5, b"FOO"));
-//     next!(lex, label!(Inst::AddrLabel, 5, 1, b"BARBAZ"));
-//     next!(lex, Inst::AddrKBD(loc!(9, 5)));
-//     next!(lex, label!(Inst::AddrLabel, 11, 1, b"BAZOO"));
-//     next!(lex, Inst::AddrLCL(loc!(13, 1)));
-//     next!(lex, label!(Inst::AddrLabel, 13, 6, b"LCLCL"));
-//     next!(lex, Inst::AddrSCREEN(loc!(14, 1)));
-//     next!(lex, Inst::AddrSP(loc!(14, 9)));
-//     next!(lex, label!(Inst::AddrLabel, 14, 13, b"SPP"));
-//     next!(lex, Inst::AddrARG(loc!(15, 1)));
-//     next!(lex, label!(Inst::AddrLabel, 15, 6, b"ARG0"));
-//     next!(lex, Inst::AddrTHIS(loc!(16, 1)));
-//     next!(lex, Inst::AddrTHAT(loc!(16, 7)));
-//     next!(lex, label!(Inst::AddrLabel, 16, 13, b"THOSE"));
-//     next!(lex, Inst::AddrR0(loc!(17, 1)));
-//     next!(lex, Inst::AddrR1(loc!(17, 5)));
-//     next!(lex, Inst::AddrR11(loc!(17, 9)));
-//     next!(lex, label!(Inst::AddrLabel, 17, 14, b"R1_hello"));
-//     next!(lex, label!(Inst::AddrLabel, 17, 24, b"R11_hello"));
-//     assert_eq!(lex.next(), None);
-//   }
+  //   #[test]
+  //   fn address_labels() {
+  //     let mut lex = lex!("addr_labels");
+  //     next!(lex, label!(Inst::AddrLabel, 3, 5, b"FOO"));
+  //     next!(lex, label!(Inst::AddrLabel, 5, 1, b"BARBAZ"));
+  //     next!(lex, Inst::AddrKBD(loc!(9, 5)));
+  //     next!(lex, label!(Inst::AddrLabel, 11, 1, b"BAZOO"));
+  //     next!(lex, Inst::AddrLCL(loc!(13, 1)));
+  //     next!(lex, label!(Inst::AddrLabel, 13, 6, b"LCLCL"));
+  //     next!(lex, Inst::AddrSCREEN(loc!(14, 1)));
+  //     next!(lex, Inst::AddrSP(loc!(14, 9)));
+  //     next!(lex, label!(Inst::AddrLabel, 14, 13, b"SPP"));
+  //     next!(lex, Inst::AddrARG(loc!(15, 1)));
+  //     next!(lex, label!(Inst::AddrLabel, 15, 6, b"ARG0"));
+  //     next!(lex, Inst::AddrTHIS(loc!(16, 1)));
+  //     next!(lex, Inst::AddrTHAT(loc!(16, 7)));
+  //     next!(lex, label!(Inst::AddrLabel, 16, 13, b"THOSE"));
+  //     next!(lex, Inst::AddrR0(loc!(17, 1)));
+  //     next!(lex, Inst::AddrR1(loc!(17, 5)));
+  //     next!(lex, Inst::AddrR11(loc!(17, 9)));
+  //     next!(lex, label!(Inst::AddrLabel, 17, 14, b"R1_hello"));
+  //     next!(lex, label!(Inst::AddrLabel, 17, 24, b"R11_hello"));
+  //     assert_eq!(lex.next(), None);
+  //   }
 
-//   #[test]
-//   fn label() {
-//     let mut lex = lex!("label");
-//     next!(lex, label!(Inst::AddrLabel, 3, 5, b"FOO"));
-//     next!(lex, label!(Inst::Label, 5, 1, b"LABEL"));
-//     next!(lex, label!(Inst::AddrLabel, 9, 5, b"LABEL"));
-//     next!(lex, label!(Inst::AddrLabel, 11, 1, b"BAR"));
-//     next!(lex, label!(Inst::Label, 13, 1, b"BAR"));
-//     next!(lex, label!(Inst::AddrLabel, 15, 1, b"LAB0"));
-//     next!(lex, label!(Inst::Label, 17, 1, b"LAB0"));
-//     assert_eq!(lex.next(), None);
-//   }
+  //   #[test]
+  //   fn label() {
+  //     let mut lex = lex!("label");
+  //     next!(lex, label!(Inst::AddrLabel, 3, 5, b"FOO"));
+  //     next!(lex, label!(Inst::Label, 5, 1, b"LABEL"));
+  //     next!(lex, label!(Inst::AddrLabel, 9, 5, b"LABEL"));
+  //     next!(lex, label!(Inst::AddrLabel, 11, 1, b"BAR"));
+  //     next!(lex, label!(Inst::Label, 13, 1, b"BAR"));
+  //     next!(lex, label!(Inst::AddrLabel, 15, 1, b"LAB0"));
+  //     next!(lex, label!(Inst::Label, 17, 1, b"LAB0"));
+  //     assert_eq!(lex.next(), None);
+  //   }
 
-// #[test]
-// fn assignments() {
-//   let mut scanner = scanner!("assignments");
-//   assert_next!(
-//     scanner,
-//     Token::Assignment(Destination::A, Computation::MMinus1),
-//     Location::new(1, 1)
-//   );
-//   assert_next!(
-//     scanner,
-//     Token::Assignment(Destination::AM, Computation::DOrA),
-//     Location::new(2, 1)
-//   );
-//   assert_next!(
-//     scanner,
-//     Token::Assignment(Destination::AMD, Computation::APlus1),
-//     Location::new(3, 1)
-//   );
-//   assert_eq!(scanner.next(), None);
-// }
+  // #[test]
+  // fn assignments() {
+  //   let mut scanner = scanner!("assignments");
+  //   assert_next!(
+  //     scanner,
+  //     Token::Assignment(Destination::A, Computation::MMinus1),
+  //     Location::new(1, 1)
+  //   );
+  //   assert_next!(
+  //     scanner,
+  //     Token::Assignment(Destination::AM, Computation::DOrA),
+  //     Location::new(2, 1)
+  //   );
+  //   assert_next!(
+  //     scanner,
+  //     Token::Assignment(Destination::AMD, Computation::APlus1),
+  //     Location::new(3, 1)
+  //   );
+  //   assert_eq!(scanner.next(), None);
+  // }
 
-// #[test]
-// fn branches() {
-//   let mut scanner = scanner!("branches");
-//   assert_next!(scanner, tbuf, Token::Comp(Comp::MMinus1));
-//   assert_eq!(scanner.token_pos(), Location::new(1, 1));
-//   assert_next!(scanner, tbuf, Token::Semi);
-//   assert_eq!(scanner.token_pos(), Location::new(1, 4));
-//   assert_next!(scanner, tbuf, Token::Jump(Jump::JEQ));
-//   assert_eq!(scanner.token_pos(), Location::new(1, 5));
-//   assert_next!(scanner, tbuf, Token::Comp(Comp::DOrA));
-//   assert_eq!(scanner.token_pos(), Location::new(2, 1));
-//   assert_next!(scanner, tbuf, Token::Semi);
-//   assert_eq!(scanner.token_pos(), Location::new(2, 4));
-//   assert_next!(scanner, tbuf, Token::Jump(Jump::JNE));
-//   assert_eq!(scanner.token_pos(), Location::new(2, 5));
-//   assert_next!(scanner, tbuf, Token::Comp(Comp::APlus1));
-//   assert_eq!(scanner.token_pos(), Location::new(3, 1));
-//   assert_next!(scanner, tbuf, Token::Semi);
-//   assert_eq!(scanner.token_pos(), Location::new(3, 4));
-//   assert_next!(scanner, tbuf, Token::Jump(Jump::JMP));
-//   assert_eq!(scanner.token_pos(), Location::new(3, 5));
-//   assert_eq!(scanner.next(), None);
-// }
+  // #[test]
+  // fn branches() {
+  //   let mut scanner = scanner!("branches");
+  //   assert_next!(scanner, tbuf, Token::Comp(Comp::MMinus1));
+  //   assert_eq!(scanner.token_pos(), Location::new(1, 1));
+  //   assert_next!(scanner, tbuf, Token::Semi);
+  //   assert_eq!(scanner.token_pos(), Location::new(1, 4));
+  //   assert_next!(scanner, tbuf, Token::Jump(Jump::JEQ));
+  //   assert_eq!(scanner.token_pos(), Location::new(1, 5));
+  //   assert_next!(scanner, tbuf, Token::Comp(Comp::DOrA));
+  //   assert_eq!(scanner.token_pos(), Location::new(2, 1));
+  //   assert_next!(scanner, tbuf, Token::Semi);
+  //   assert_eq!(scanner.token_pos(), Location::new(2, 4));
+  //   assert_next!(scanner, tbuf, Token::Jump(Jump::JNE));
+  //   assert_eq!(scanner.token_pos(), Location::new(2, 5));
+  //   assert_next!(scanner, tbuf, Token::Comp(Comp::APlus1));
+  //   assert_eq!(scanner.token_pos(), Location::new(3, 1));
+  //   assert_next!(scanner, tbuf, Token::Semi);
+  //   assert_eq!(scanner.token_pos(), Location::new(3, 4));
+  //   assert_next!(scanner, tbuf, Token::Jump(Jump::JMP));
+  //   assert_eq!(scanner.token_pos(), Location::new(3, 5));
+  //   assert_eq!(scanner.next(), None);
+  // }
 
-// #[test]
-// fn instructions() {
-//   let mut scanner = scanner!("instructions");
-//   assert_next!(scanner, tbuf, Token::Dest(Dest::A));
-//   assert_eq!(scanner.token_pos(), Location::new(1, 1));
-//   assert_next!(scanner, tbuf, Token::Comp(Comp::MMinus1));
-//   assert_eq!(scanner.token_pos(), Location::new(1, 3));
-//   assert_next!(scanner, tbuf, Token::Semi);
-//   assert_eq!(scanner.token_pos(), Location::new(1, 6));
-//   assert_next!(scanner, tbuf, Token::Jump(Jump::JEQ));
-//   assert_eq!(scanner.token_pos(), Location::new(1, 7));
-//   assert_next!(scanner, tbuf, Token::Dest(Dest::AM));
-//   assert_eq!(scanner.token_pos(), Location::new(2, 1));
-//   assert_next!(scanner, tbuf, Token::Comp(Comp::DOrA));
-//   assert_eq!(scanner.token_pos(), Location::new(2, 4));
-//   assert_next!(scanner, tbuf, Token::Semi);
-//   assert_eq!(scanner.token_pos(), Location::new(2, 7));
-//   assert_next!(scanner, tbuf, Token::Jump(Jump::JNE));
-//   assert_eq!(scanner.token_pos(), Location::new(2, 8));
-//   assert_next!(scanner, tbuf, Token::Dest(Dest::AMD));
-//   assert_eq!(scanner.token_pos(), Location::new(3, 1));
-//   assert_next!(scanner, tbuf, Token::Comp(Comp::APlus1));
-//   assert_eq!(scanner.token_pos(), Location::new(3, 5));
-//   assert_next!(scanner, tbuf, Token::Semi);
-//   assert_eq!(scanner.token_pos(), Location::new(3, 8));
-//   assert_next!(scanner, tbuf, Token::Jump(Jump::JMP));
-//   assert_eq!(scanner.token_pos(), Location::new(3, 9));
-//   assert_eq!(scanner.next(), None);
-// }
-//}
+  // #[test]
+  // fn instructions() {
+  //   let mut scanner = scanner!("instructions");
+  //   assert_next!(scanner, tbuf, Token::Dest(Dest::A));
+  //   assert_eq!(scanner.token_pos(), Location::new(1, 1));
+  //   assert_next!(scanner, tbuf, Token::Comp(Comp::MMinus1));
+  //   assert_eq!(scanner.token_pos(), Location::new(1, 3));
+  //   assert_next!(scanner, tbuf, Token::Semi);
+  //   assert_eq!(scanner.token_pos(), Location::new(1, 6));
+  //   assert_next!(scanner, tbuf, Token::Jump(Jump::JEQ));
+  //   assert_eq!(scanner.token_pos(), Location::new(1, 7));
+  //   assert_next!(scanner, tbuf, Token::Dest(Dest::AM));
+  //   assert_eq!(scanner.token_pos(), Location::new(2, 1));
+  //   assert_next!(scanner, tbuf, Token::Comp(Comp::DOrA));
+  //   assert_eq!(scanner.token_pos(), Location::new(2, 4));
+  //   assert_next!(scanner, tbuf, Token::Semi);
+  //   assert_eq!(scanner.token_pos(), Location::new(2, 7));
+  //   assert_next!(scanner, tbuf, Token::Jump(Jump::JNE));
+  //   assert_eq!(scanner.token_pos(), Location::new(2, 8));
+  //   assert_next!(scanner, tbuf, Token::Dest(Dest::AMD));
+  //   assert_eq!(scanner.token_pos(), Location::new(3, 1));
+  //   assert_next!(scanner, tbuf, Token::Comp(Comp::APlus1));
+  //   assert_eq!(scanner.token_pos(), Location::new(3, 5));
+  //   assert_next!(scanner, tbuf, Token::Semi);
+  //   assert_eq!(scanner.token_pos(), Location::new(3, 8));
+  //   assert_next!(scanner, tbuf, Token::Jump(Jump::JMP));
+  //   assert_eq!(scanner.token_pos(), Location::new(3, 9));
+  //   assert_eq!(scanner.next(), None);
+  // }
+}
