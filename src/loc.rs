1abc7bd02bed4491d84906d5dc832ebf67716e18
Fred Morcos <fm@fredmorcos.com>
2020-12-16 19:37:22 +0100

    Document crate::loc

--- src/loc.rs
+++ src/loc.rs
@@ -1,15 +1,30 @@
+//! Structures related to locations in source code.
+//!
+//! This module offers [SrcLoc] for locations in HAS's sources and
+//! [Loc] for locations in input programs. The macros [srcloc!] and
+//! [loc!] can respectively be used as shorthands to create these
+//! structures.
+
 use std::fmt;
 
+/// Denotes locations in the HAS crates sources (e.g. to track where
+/// in the HAS sources error objects were created).
+///
+/// # impl `Display` examples
+///
+/// ```
+/// use has::loc::SrcLoc;
+///
+/// let src_loc = SrcLoc::new("foo.asm", 30);
+/// assert_eq!(format!("{}", src_loc), "file foo.asm, line 30");
+/// ```
+#[doc(alias = "sourcelocation")]
+#[doc(alias = "sourceloc")]
+#[doc(alias = "srclocation")]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SrcLoc {
   file: &'static str,
   line: u32,
-}
-
-impl SrcLoc {
-  pub fn new(file: &'static str, line: u32) -> Self {
-    Self { file, line }
-  }
 }
 
 impl fmt::Display for SrcLoc {
@@ -18,84 +33,63 @@ impl fmt::Display for SrcLoc {
   }
 }
 
+impl SrcLoc {
+  /// Creates a new [SrcLoc] object. You probably want to use the
+  /// provided [srcloc!()] macro instead.
+  ///
+  /// # Arguments
+  ///
+  /// * `file` - Filename which would typically be created by a call
+  /// to [core::file!()].
+  ///
+  /// * `line` - Line in `file` which Would typically be created by a
+  /// call to [core::line!()].
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use has::loc::SrcLoc;
+  ///
+  /// let src_loc = SrcLoc::new("foo.asm", 26);
+  /// ```
+  pub fn new(file: &'static str, line: u32) -> Self {
+    Self { file, line }
+  }
+
+  /// Returns the filename of a [SrcLoc] object.
+  pub fn file(&self) -> &'static str {
+    self.file
+  }
+
+  /// Returns the line of a [SrcLoc] object.
+  pub fn line(&self) -> u32 {
+    self.line
+  }
+}
+
+/// Shorthand to create a [SrcLoc] object that denotes the current
+/// call-site's location.
+///
+/// # Examples
+///
+/// ```
+/// use has::loc::SrcLoc;
+/// use has::srcloc;
+///
+/// let src_loc = srcloc!();
+/// assert_ne!(src_loc, srcloc!());
+/// assert_eq!(srcloc!(), SrcLoc::new(file!(), line!()));
+/// ```
+#[doc(alias = "source location")]
+#[doc(alias = "source location macro")]
+#[doc(alias = "source loc macro")]
+#[doc(alias = "source loc")]
+#[doc(alias = "src loc")]
+#[doc(alias = "src loc macro")]
+#[doc(alias = "srcloc macro")]
 #[macro_export]
 macro_rules! srcloc {
   () => {
     SrcLoc::new(file!(), line!())
   };
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Loc {
-  line: usize,
-  col: usize,
-}
-
-impl Loc {
-  pub fn inc(&mut self, byte: u8) {
-    if byte == b'\n' {
-      self.line += 1;
-      self.col = 0;
-    } else {
-      self.col += 1;
-    }
-  }
-}
-
-impl Default for Loc {
-  fn default() -> Self {
-    Self { line: 1, col: 0 }
-  }
-}
-
-impl fmt::Display for Loc {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "line {}, column {}", self.line, self.col)
-  }
-}
-
-#[cfg(test)]
-impl Loc {
-  pub fn new(line: usize, col: usize) -> Self {
-    Self { line, col }
-  }
-}
-
-#[macro_export]
-macro_rules! loc {
-  () => {
-    Loc::default()
-  };
-  ($line:expr, $col:expr) => {
-    Loc::new($line, $col)
-  };
-}
-
-#[cfg(test)]
-mod tests {
-  use super::Loc;
-
-  #[test]
-  fn with_newline() {
-    let mut loc = loc!();
-    loc.inc(b'a');
-    assert_eq!(loc, loc!(1, 1));
-    loc.inc(b'\n');
-    assert_eq!(loc, loc!(2, 0));
-    loc.inc(b'b');
-    assert_eq!(loc, loc!(2, 1));
-    loc.inc(b'c');
-    assert_eq!(loc, loc!(2, 2));
-  }
-
-  #[test]
-  fn without_newline() {
-    let mut loc = loc!();
-    loc.inc(b'a');
-    assert_eq!(loc, loc!(1, 1));
-    loc.inc(b'b');
-    assert_eq!(loc, loc!(1, 2));
-    loc.inc(b'c');
-    assert_eq!(loc, loc!(1, 3));
-  }
 }
