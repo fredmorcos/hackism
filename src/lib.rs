0ba48a50182e97a09add93c1f750f1d2eb67330e
Fred Morcos <fm@fredmorcos.com>
2020-12-22 23:51:06 +0100

    Crate lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 #![warn(clippy::all)]
 
+//! **HAS** stands for the **H**ACK **A**pplication **S**uite. It
+//! consists of a library and binaries to handle the HACK machine
+//! language and the JACK programming language. Currently this crate
+//! only contains a HACK assembler and few library utilities. The HACK
+//! assembler translates HACK assembly code to HACK machine code.
+
+pub mod inst;
+pub mod utils;
+
 mod gen;
-mod inst;
 mod lex;
-mod loc;
 mod prog;
