f2fbbeca2c1fbf8a4cb8ed69ac9799c2282a8329
Fred Morcos <fm@fredmorcos.com>
2020-12-23 22:47:29 +0100

    Implement read_while on Byte and Buf

--- src/utils.rs
+++ src/utils.rs
@@ -1,21 +1,26 @@
-/// Splits a string `s` at the point when `p` returns false for a character.
+pub type Byte = u8;
+pub type Buf<'b> = &'b [Byte];
+
+/// Splits a buffer `b` at the point when `p` returns false for a
+/// byte.
 ///
-/// Returns the parsed string and the remainder.
+/// Returns the parsed part of the buffer and the remainder.
 ///
 /// # Arguments
 ///
-/// * `s` - The input string.
+/// * `b` - The input buffer.
 ///
 /// * `p` - A predicate that should return true as long as
 ///         `read_while` should continue to consume input.
-pub fn read_while<P>(s: &str, p: P) -> (&str, &str)
+pub fn read_while<P>(b: Buf, p: P) -> (Buf, Buf)
 where
-  P: Fn(char) -> bool,
+  P: Fn(Byte) -> bool,
 {
-  let split = s
-    .char_indices()
-    .find_map(|(i, c)| if p(c) { None } else { Some(i) })
-    .unwrap_or_else(|| s.len());
+  let split = b
+    .iter()
+    .enumerate()
+    .find_map(|(i, &c)| if p(c) { None } else { Some(i) })
+    .unwrap_or_else(|| b.len());
 
-  (&s[..split], &s[split..])
+  (&b[..split], &b[split..])
 }
