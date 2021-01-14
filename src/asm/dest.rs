1365054423fd1e15fa308e3a77d6e8a163316269
Fred Morcos <fm@fredmorcos.com>
2021-01-14 21:07:55 +0100

    Dest from 16-bit int

--- src/asm/dest.rs
+++ src/asm/dest.rs
@@ -77,6 +77,24 @@ impl From<Dest> for u16 {
   }
 }
 
+impl TryFrom<u16> for Dest {
+  type Error = ();
+
+  fn try_from(v: u16) -> Result<Self, Self::Error> {
+    match v {
+      0b000 => Ok(Dest::Null),
+      0b001 => Ok(Dest::M),
+      0b010 => Ok(Dest::D),
+      0b011 => Ok(Dest::MD),
+      0b100 => Ok(Dest::A),
+      0b101 => Ok(Dest::AM),
+      0b110 => Ok(Dest::AD),
+      0b111 => Ok(Dest::AMD),
+      _ => Err(()),
+    }
+  }
+}
+
 impl fmt::Display for Dest {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     match self {
