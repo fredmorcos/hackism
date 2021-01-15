20c12086a26086622f7e38397ad7cb542a327d7b
Fred Morcos <fm@fredmorcos.com>
2021-01-15 17:08:02 +0100

    Disassembler fixtures test

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -1,8 +1,9 @@
 #![warn(clippy::all)]
 
 #[cfg(test)]
-mod asm_fixtures {
+mod fixtures {
   use has::asm;
+  use has::dis;
 
   use std::convert::TryFrom;
   use std::fs;
@@ -12,7 +13,7 @@ mod asm_fixtures {
   use std::io::Write;
 
   #[test]
-  fn test_programs() {
+  fn assembler() {
     for file in fs::read_dir("tests/fixtures").unwrap() {
       let file = file.unwrap();
       let mut file_path = file.path();
@@ -36,6 +37,40 @@ mod asm_fixtures {
           for inst in prog.text_encoder() {
             let inst = &inst.unwrap();
             writer.write_all(inst).unwrap();
+            writer.write_all(&[b'\n']).unwrap();
+          }
+        }
+
+        assert_eq!(&output, &fixture);
+      }
+    }
+  }
+
+  #[test]
+  fn disassembler() {
+    for file in fs::read_dir("tests/fixtures").unwrap() {
+      let file = file.unwrap();
+      let mut file_path = file.path();
+      let file_ext = file_path.extension().unwrap().to_str().unwrap();
+
+      if file_ext == "hack" {
+        println!("Testing fixture {}", file_path.display());
+
+        let mut input = Vec::with_capacity(1024);
+        File::open(&file_path).unwrap().read_to_end(&mut input).unwrap();
+        let mut prog = dis::prog::Prog::new_text(input.as_slice()).unwrap();
+
+        file_path.set_extension("dis");
+        let mut fixture = Vec::with_capacity(1024);
+        File::open(&file_path).unwrap().read_to_end(&mut fixture).unwrap();
+
+        let mut output = Vec::with_capacity(1024);
+        {
+          let mut writer = BufWriter::new(&mut output);
+
+          for inst in prog.decoder() {
+            let inst = &inst.unwrap();
+            writer.write_all(inst.as_bytes()).unwrap();
             writer.write_all(&[b'\n']).unwrap();
           }
         }
