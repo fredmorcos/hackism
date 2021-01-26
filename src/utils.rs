afc14da12c82ef0e18c8763608b7d46954d5b1a6
Fred Morcos <fm@fredmorcos.com>
2021-01-26 19:19:42 +0100

    Document u16_bintext

--- src/utils.rs
+++ src/utils.rs
@@ -111,6 +111,12 @@ where
   }
 }
 
+/// Converts a [u16] value into 16 ASCII bytes representing the binary
+/// representations of the value.
+///
+/// # Arguments
+///
+/// * `val` - The value to be converted.
 pub fn u16_bintext(val: u16) -> [u8; 16] {
   const VALS: [u8; 2] = [b'0', b'1'];
   [
