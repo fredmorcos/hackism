c9eae403fbc84e39acc76ea8e47f5ad8d62739a3
Fred Morcos <fm@fredmorcos.com>
2021-01-29 15:17:51 +0100

    Doc update

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,5 @@
-//! Submodules for parsing, encoding and decoding helpers. A utilities
-//! package that may contain things that are not directly related to
-//! HACK and JACK.
+//! Submodules for parsing and encoding helpers. Utilities that may
+//! not be directly related to HACK and JACK.
 
 pub mod buf;
 pub mod conv;
